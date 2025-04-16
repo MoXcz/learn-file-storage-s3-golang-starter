bb31f787ed7d9c5019d3ac15d668462f6d4dee1b
mocos <130593875+MoXcz@users.noreply.github.com>
2025-04-15 21:48:31 -0400

    feat: Add caching for thumbnails

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,8 +1,16 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base64"
+	"errors"
 	"fmt"
+	"io"
+	"mime"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
@@ -28,10 +36,81 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
 
-	// TODO: implement the upload here
+	const maxMemory = 10 << 20 // 10 MB
+	err = r.ParseMultipartForm(maxMemory)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not parse form contents", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	file, header, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusBadRequest, "Unable to parse form file", err)
+		return
+	}
+	defer file.Close()
+
+	contentType := header.Header.Get("Content-Type")
+	mediaType, _, err := mime.ParseMediaType(contentType)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not get media type", nil)
+		return
+	}
+
+	if mediaType != "image/jpeg" && mediaType != "image/png" {
+		respondWithError(w, http.StatusBadRequest, "Invalid media type", nil)
+		return
+	}
+
+	if contentType == "" {
+		respondWithError(w, http.StatusBadRequest, "Missing content type header", nil)
+		return
+	}
+
+	_, fileExtension, _ := strings.Cut(contentType, "/")
+	randID := make([]byte, 32)
+	_, err = rand.Read(randID)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create resource ID", nil)
+		return
+	}
+
+	resourceID := base64.RawURLEncoding.EncodeToString((randID))
+
+	dataURL := fmt.Sprintf("%s.%s", resourceID, fileExtension) // <videoID>.<file_extension>
+	filepath := filepath.Join(cfg.assetsRoot, dataURL)
+	createdFile, err := os.Create(filepath)
+	defer createdFile.Close()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not create video file", nil)
+		return
+	}
+
+	_, err = io.Copy(createdFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not save file", nil)
+		return
+	}
+
+	video, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		return
+	}
+	if userID != video.UserID {
+		respondWithError(w, http.StatusUnauthorized, "Current user can't modify this video", errors.New("Incorrect auth"))
+		return
+	}
+
+	thumbnailURL := fmt.Sprintf("http://localhost:%s/%s", cfg.port, filepath)
+	video.ThumbnailURL = &thumbnailURL
+
+	err = cfg.db.UpdateVideo(video)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Could not update thumbnail", err)
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, video)
 }
